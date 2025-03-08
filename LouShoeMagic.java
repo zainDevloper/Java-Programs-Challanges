cd6357c64f69ee700787f1d5df5125038a638afb
Zain Developer <zainulabdeen34554@gmail.com>
2025-03-08 08:32:32 +0500

    Upload LouShoeMagic.java
    Added a Java program that initializes a 3x3 matrix and iterates through its elements. The program serves as a foundation for further modifications, such as performing calculations on the matrix.

---
+++ LouShoeMagic.java
@@ -0,0 +1,13 @@
+public class LouShoeMagic {
+    
+    public static void main(String[] args) {
+        int[][] magic={{1,2,3},{4,5,6},{7,8,9}};
+        for(int a=0;a<=2;a++){ 
+            for(int j=0;j<=2;j++){
+                int total=magic[a][j];
+            }
+        }
+    }
+}
+
+ 
